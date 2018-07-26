f33ea834b0cfdc1f76299c9d6eb8c7bd266da726
王杰 <1247535095@qq.com>
2018-07-26 20:36:11 +0800

    项目的基本搭建

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+
+// 引入css
+import './assets/statics/site/css/style.css';
+
+Vue.config.productionTip = false
+
+new Vue({
+  //选择器
+   el:'#app',
+   //渲染app组件
+  render: h => h(App)
+})
